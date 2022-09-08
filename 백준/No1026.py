05de2da7a2f896d2fadf16e7860f83ca9705afe2
sohyun-99 <sophia4130@naver.com>
2022-09-08 19:18:06 +0900

    no1026

--- 백준/No1026.py
+++ 백준/No1026.py
@@ -1,16 +1,23 @@
-N=input("")
+N=int(input()) ##애초에 int로 입력값 받기
 A=list(map(int,input().split()))
 B=list(map(int,input().split()))
 A.sort() # A값을 오름차순으로 정렬
 
-Max_index=0
 result=0
-for i in range(int(N)):
-    Max=0
-    for n in range(len(B)):
-        if B[n-1]>=Max:
-            Max=B[n-1]
-            Max_index=n-1
-    B.pop(Max_index)
-    result+=Max*A[i]
-print(result)
+for i in range(N):
+    result+=A[i]*max(B)
+    B.pop(B.index(max(B)))
+print(result)    
+
+##초기 알고리즘 .. 바보처럼 max, min 함수 안씀...수동으로 찾아줌
+##Max_index=0
+##result=0
+##for i in range(N):
+##    Max=0
+##    for n in range(len(B)):
+##        if B[n-1]>=Max:
+##            Max=B[n-1]
+##            Max_index=n-1
+##    B.pop(Max_index)
+##    result+=Max*A[i]
+##print(result)
