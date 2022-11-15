d4a86f16e2b49ab785cb450d35e09fd582eda7ac
sohyun-99 <sophia4130@naver.com>
2022-11-15 21:02:54 +0900

    221115

--- 백준/coteAlgorithm/ch5 DFS BFS/5-7.py
+++ 백준/coteAlgorithm/ch5 DFS BFS/5-7.py
@@ -1,2 +1,12 @@
 #행(row)이 3개인 2차원 리스트로 인접 리스트 표현
 graph=[[] for _ in range(3)]
+
+#노드 0에 연결된 노드 정보 저장 (노드, 거리)
+graph[0].append((1,7))
+graph[0].append((2,5))
+
+graph[1].append((0,7))
+
+graph[2].append((1,5))
+
+print(graph)
